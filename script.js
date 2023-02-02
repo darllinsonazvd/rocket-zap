7f176cc3816929bb1df32816b1cd8015cbec654c
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2023-02-02 11:51:54 -0300

    Add text verification

--- script.js
+++ script.js
@@ -4,23 +4,28 @@ const messageInput = document.querySelector('.input-wrapper input');
 messages.lastElementChild.scrollIntoView();
 
 function sendMessage() {
-  let newMessage = document.createElement('div');
-  newMessage.classList.add('message');
-  newMessage.classList.add('you');
+  if (messageInput.value) {
+    const newMessage = document.createElement('div');
+    newMessage.classList.add('message');
+    newMessage.classList.add('you');
 
-  let newMessageTitle = document.createElement('div');
-  let title = document.createTextNode(`Você - ${new Date().getHours()}:${new Date().getMinutes()}`);
-  newMessageTitle.appendChild(title);
-  newMessageTitle.classList.add('top');
+    const newMessageTitle = document.createElement('div');
+    const title = document.createTextNode(
+      `Você - ${new Date().getHours()}:${new Date().getMinutes()}`,
+    );
+    newMessageTitle.appendChild(title);
+    newMessageTitle.classList.add('top');
 
-  let newMessageBody = document.createElement('div');
-  let messageContent = document.createTextNode(messageInput.value);
-  newMessageBody.appendChild(messageContent);
-  newMessageBody.classList.add('body');
+    const newMessageBody = document.createElement('div');
+    const messageContent = document.createTextNode(messageInput.value);
+    newMessageBody.appendChild(messageContent);
+    newMessageBody.classList.add('body');
 
-  newMessage.appendChild(newMessageTitle);
-  newMessage.appendChild(newMessageBody);
+    newMessage.appendChild(newMessageTitle);
+    newMessage.appendChild(newMessageBody);
 
-  messages.appendChild(newMessage);
-  messages.lastElementChild.scrollIntoView();
+    messages.appendChild(newMessage);
+    messageInput.value = '';
+    messages.lastElementChild.scrollIntoView();
+  }
 }
